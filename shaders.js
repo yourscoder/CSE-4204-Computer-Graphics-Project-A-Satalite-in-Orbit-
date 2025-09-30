427fc6b9760e9293c14100239089ebe05b6a88a7
yourscoder <98487829+yourscoder@users.noreply.github.com>
2025-09-30 13:01:11 +0600

    Update shaders.js

--- shaders.js
+++ shaders.js
@@ -5,3 +5,15 @@ export const atmosphereVertexShader = `
     gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }
 `;
+
+export const atmosphereFragmentShader = `
+  varying vec3 vNormal;
+  uniform vec3 glowColor;
+  void main() {
+    
+    float intensity = pow(0.6 - dot(vNormal, vec3(0.0, 0.0, 1.0)), 3.0);
+    
+    intensity = smoothstep(0.0, 1.0, intensity);
+    gl_FragColor = vec4(glowColor * intensity, intensity);
+  }
+`;  
